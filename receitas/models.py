e31dc8ec79b6735865b344de1d91fadeea1eb1c1
Arthur <arthurdevcontatoo@gmail.com>
2025-07-25 11:33:32 -0300

    criando models e rodando migrate

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Receita(models.Model): #Sempre que for fazer a criação de uma model, ela precisa receber como herança essa classe models.Model
+    nome_receita = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.IntegerField()
+    rendimento = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    data_receita = models.DateTimeField(default=datetime.now, blank=True)
+
+    
